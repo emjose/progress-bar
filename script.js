2dae193c44e3c0b294eace13f2c4a96319eae8f3
emmanueljose <emjose@gmail.com>
2022-09-23 21:03:56 -0400

    update structure and styling

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+"use strict";
 
-let currentActive = 1
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-next.addEventListener('click', () => {
-    currentActive++
+let currentActive = 1;
 
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
+next.addEventListener("click", () => {
+	currentActive++;
 
-    update()
-})
+	if (currentActive > circles.length) {
+		currentActive = circles.length;
+	}
 
-prev.addEventListener('click', () => {
-    currentActive--
+	update();
+});
 
-    if(currentActive < 1) {
-        currentActive = 1
-    }
+prev.addEventListener("click", () => {
+	currentActive--;
 
-    update()
-})
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+
+	update();
+});
 
 function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
+	circles.forEach((circle, idx) => {
+		if (idx < currentActive) {
+			circle.classList.add("active");
+		} else {
+			circle.classList.remove("active");
+		}
+	});
 
-    const actives = document.querySelectorAll('.active')
+	const actives = document.querySelectorAll(".active");
 
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+	progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
 
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
-    } else {
-        prev.disabled = false
-        next.disabled = false
-    }
+	if (currentActive === 1) {
+		prev.disabled = true;
+	} else if (currentActive === circles.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
 }
